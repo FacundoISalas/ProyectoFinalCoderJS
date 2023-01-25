cab47e0a2575e444e32fffd1bc69cf5041fd4b8e
Facundo Salas <fsalas@qplus.com.ar>
2023-01-25 19:43:27 -0300

    localstorage funcional

--- js/base.js
+++ js/base.js
@@ -1,31 +1,95 @@
+// variables globales 
+let = resultados = [];
+let seriesAñadidas = [];
+
+// localstorage start
+function incializeLocalStorage() {
+  const localdata = localStorage.getItem('listadoseries');
+  if (localdata) {
+    seriesAñadidas = JSON.parse(localdata);
+  }
+};
+
+incializeLocalStorage();
+
+// renderizar listado series a ver
 // ENDPOINTS API TMDB
 const optionsGet = {
 	method: 'GET',
 };
+async function detalleSerie(id) {
+  const response = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=c675bcfb72f422cb8d13272f97429e98&language=es-US&page=1&include_adult=false`, optionsGet)
+    .then(res => res.json())
+    .catch(err => console.error(err));
+    const data = await response;
+    return data;
+};
 
-function buscarSerie() {
+async function buscarSerie() {
     const valorBusqueda = document.getElementById('paramBusqueda').value;
     fetch(`https://api.themoviedb.org/3/search/tv?api_key=c675bcfb72f422cb8d13272f97429e98&language=es-US&page=1&query=${valorBusqueda}&include_adult=false`, optionsGet)
 	.then(res => res.json())
-        .then(data => {
+        .then(async data => {
+          document.getElementById('contenidoBusqueda').innerHTML = '';
             let htmltorender = '<br>';
             const objData = data.results;
-            console.log('datatrue', objData);
             for (let i = 0; i < objData.length; i++) {
                 const e = objData[i];
+                const details = await detalleSerie(e.id);
+                e.detail = details;
+                e.ultimoEpisodioVisto = null;
                 const htmlData = `<div class="gridCard my-5">
                 <div>
                   <img src="https://image.tmdb.org/t/p/w300${e.poster_path}" alt="">
                 </div>
                 <div>
-                  <h3>title goes here</h3>
-                  <p>somedata</p>
-                  <p>even some more other data</p>
+                  <h3>${e.name}</h3>
+                  <span id="${e.id}" style="display:none;"></span>
+                  <p>Califacion: ${e.vote_average === 0 ? 'Sin Califacion' : e.vote_average} </p>
+                  <p>Cantidad de episodios: ${e.detail.number_of_episodes}</p>
+                  <p> Ultimo capitulo emitido: ${e.detail.last_episode_to_air ? e.detail.last_episode_to_air.episode_number : '-'} </p>
+                  <p>Fecha emision ultimo episodio: ${e.detail.last_air_date ? dayjs(e.detail.last_air_date).format('DD/MM/YYYY') : '-'} </p>
+                  <p>Fecha emision proximo episodio: ${e.detail.next_episode_to_air ? dayjs(e.detail.next_episode_to_air.air_date).format('DD/MM/YYYY') : 'Finalizado/Suspendido'} </p>
+                  <div class="row justify-content-center">
+                    <div class="col-lg-12 col-md-4 col-sm-12">
+                      <div class="mb-3">
+                        <label for="ultimoCapVisto" class="form-label"
+                          >Ultimo capitulo visto</label
+                        >
+                        <input
+                          type="number"
+                          class="form-control"
+                          name="ultimoCapVisto"
+                          id="ultimoCapVisto${e.id}"
+                        />
+                      </div>
+                      <div class="col-lg-8 col-md-2 col-sm-12 d-flex flex-row justify-content-end">
+                        <button type="button" class="btn btn-primary" onclick="AñadirSerie(${e.id})">Añadir</button>
+                      </div>
+                    </div>
+                      
+                  </div>
                 </div>
               </div>`
                 htmltorender = htmltorender + htmlData;
                 document.getElementById('contenidoBusqueda').innerHTML = htmltorender;
+                resultados.push(e);
             }
          })
         .catch(err => console.error(err));
+        console.log(resultados);
+};
+
+function AñadirSerie(param) {
+  const filtro = seriesAñadidas.filter((e) => param === e.id)
+  if (filtro.length === 0) {
+    const element = resultados.filter((e) => param === e.id);
+    const getValueUltimoEpisodioVisto = document.getElementById(`ultimoCapVisto${param}`).value;
+    element[0].ultimoEpisodioVisto = getValueUltimoEpisodioVisto;
+    seriesAñadidas.push(element[0]);
+    localStorage.setItem('listadoseries', JSON.stringify(seriesAñadidas));
+    console.log('seriesAñadidasFinal', seriesAñadidas);
+  } else {
+    alert('La serie que intentas añadir ya fue añadida previamente');
+  }
 };
